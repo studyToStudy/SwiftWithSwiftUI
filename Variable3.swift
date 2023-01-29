36509e4badafca09ad8458abb7dfe9b90b761df5
leejunghoo515@naver.com <leejunghoo515@naver.com>
2023-01-29 12:45:57 +0900

    연산자

--- Variable3.swift
+++ Variable3.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct Variable3: View {
+    
+    // MARK: 대입 연산자
+    let name = "Hoo"
+    var age = 27
+    var anyNumber = 6
+    var anyNumber2 = 8
+    var anyNumber3 = 6
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            // MARK: 사칙 연산자
+//            Text("\(age + anyNumber)")
+//            Text("\(age - anyNumber)")
+//            Text("\(age * anyNumber)")
+//            Text("\(age % anyNumber)")
+//            Text("\(age / anyNumber)")
+            
+            // MARK: 비교 연산자
+            // description -> 설명값으로 해주어야 bool값이 출력된다.
+            // 6은 8보다 크다? -> false
+            // 6은 8보다 크거나 같다? -> false
+            // 6은 8보다 작다? -> true
+            // 6은 8보다 작거나 같다? -> true
+            Text("\(anyNumber > anyNumber2)".description)
+            Text("\(anyNumber >= anyNumber2)".description)
+            Text("\(anyNumber < anyNumber2)".description)
+            Text("\(anyNumber <= anyNumber2)".description)
+            Text("\(anyNumber == anyNumber3)".description)
+            Text("\(anyNumber != anyNumber2)".description)
+        }
     }
 }
 
